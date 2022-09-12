b7b73c01c8351fe66a9768d82d839c7494567a06
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-09-12 23:49:59 +0300

    task3

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,41 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+//     Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+//     Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+const galeryListRef = document.querySelector(".gallery");
+// console.log(galeryListRef);
+
+const galeryItemsEl = images
+  .map(
+    ({ url, alt }) =>
+      `<li><img src="${url}" alt="${alt}" width="420" height="280"/></li>`
+  )
+  .join("");
+
+galeryListRef.style.display = "flex";
+galeryListRef.style.justifyContent = "space-between";
+galeryListRef.style.listStyle = "none";
+galeryListRef.style.paddingLeft = "0";
+
+// console.log(galeryItemsEl);
+galeryListRef.insertAdjacentHTML("beforeend", galeryItemsEl);
