b96b9601dcfacd8a40f857094e706a7a72a8b87e
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-09-18 00:13:57 +0300

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
+const inputRef = document.querySelector("input");
+const buttonCreateRef = document.querySelector("[data-create]");
+const buttonDestroyRef = document.querySelector("[data-destroy]");
+const boxesRef = document.querySelector("#boxes");
+
+inputRef.addEventListener("change", (element) => {
+  return element.currentTarget.value;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+buttonCreateRef.addEventListener("click", createBoxes);
+
+const markup = [];
+
+function createBoxes() {
+  for (let i = 1; i <= inputRef.value; i += 1) {
+    const markupEl = `<div style="background-color:${getRandomHexColor()}; width:${
+      20 + i * 10
+    }px; height:${20 + i * 10}px"></div>`;
+    markup.push(markupEl);
+  }
+
+  boxesRef.innerHTML = markup.join("");
+}
+
+buttonDestroyRef.addEventListener("click", (event) => {
+  boxesRef.innerHTML = "";
+});
